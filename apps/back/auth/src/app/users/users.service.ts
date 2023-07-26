e46de5f03f3d33c4c809626486921120bce9197a
Leon <gabriel.leonbs@gmail.com>
2023-07-25 22:24:52 -0300

    feat/Add create and find users

--- apps/back/auth/src/app/users/users.service.ts
+++ apps/back/auth/src/app/users/users.service.ts
@@ -1,29 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './schema/users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
-interface User{
-    userId: number,
-    username: string,
-    password: string,
-}
-
-
+//fazer DTO e os tipos
+//fazer a controller de usuário
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    private readonly users = [
-        {
-          userId: 1,
-          username: 'john',
-          password: 'changeme',
-        },
-        {
-          userId: 2,
-          username: 'maria',
-          password: 'guess',
-        },
-      ];
-    
-      async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
-      }
-    }
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = this.userModel.create(createUserDto);
+    return createdUser;
+  }
+
+  
+  async findOne(name: string) {
+    const findedUser = this.userModel.findOne({name : name})
+    return findedUser;
+  }
+
+  async findAll() {
+    return this.userModel.find();
+  }
+}
