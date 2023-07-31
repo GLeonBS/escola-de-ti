4a80295e96fbdac5bd908d32c632577de6ceddd7
GLeonBS <gabriel.leonbs@gmail.com>
2023-07-31 20:38:30 -0300

    feat: 🎸 Add form products

--- libs/store/feature-products/src/lib/store-feature-products.component.ts
+++ libs/store/feature-products/src/lib/store-feature-products.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'store-store-feature-products',
   templateUrl: './store-feature-products.component.html',
   styleUrls: ['./store-feature-products.component.scss'],
 })
-export class StoreFeatureProductsComponent {}
+export class StoreFeatureProductsComponent {
+  form = new FormGroup({
+    name: new FormControl(),
+    description: new FormControl(),
+    price:new FormControl(),
+    quantity:new FormControl(),
+  })
+
+  onSubmit() {
+    
+  }
+}
--- libs/store/feature-products/src/lib/store-feature-products.module.ts
+++ libs/store/feature-products/src/lib/store-feature-products.module.ts
@@ -3,12 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { storeFeatureProductsRoutes } from './store-feature-products.routes';
 import { StoreFeatureProductsComponent } from './store-feature-products.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(storeFeatureProductsRoutes), MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(storeFeatureProductsRoutes),
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+    ],
   declarations: [StoreFeatureProductsComponent],
 })
-export class StoreFeatureProductsModule {}
+export class StoreFeatureProductsModule { }
